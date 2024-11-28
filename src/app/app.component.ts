4a2f279c4f4990bd9da1295f522dec96a3fcc3b6
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-11-28 19:42:28 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Africa';
+
+  constructor(private fb: FacebookService) {
+    const initParams: InitParams = {
+      appId: '909595094480508',
+      xfbml: true,
+      version: 'v21.0'
+    };
+
+    fb.init(initParams);
+  }
+
+  loginWithFacebook(): void {
+    this.fb.login()
+      .then((response: LoginResponse) => console.log(response))
+      .catch((error: any) => console.error(error));
+  }
 }
